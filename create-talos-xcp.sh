8126e6704b9c18d3fb14938bb013c6fb2ea10140
yuriy <ysoft2000@gmail.com>
2025-10-27 00:03:51 +0300

    metal-iso

--- create-talos-xcp.sh
+++ create-talos-xcp.sh
@@ -187,6 +187,8 @@ create_seed_iso_from_mc() {
   # Создаем полный machineconfig
   local config
   config=$(yq '.machine.network.hostname = "'"${vmname}"'"' "$config_file" | \
+  yq '.machine.network.interfaces[0].interface = "eth0"' | \
+  yq '.machine.network.interfaces[0].dhcp = false' | \
   yq '.machine.network.interfaces[0].routes[0].gateway = "'"${GATEWAY}"'"' | \
   yq '.machine.network.interfaces[0].addresses[0] = "'"$ip_cidr"'"' | \
   yq '.machine.time.servers[0] = "pool.ntp.org"' | \
