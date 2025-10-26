b9785f269ee7505e673aefbb028ee3f3c32c099c
yuriy <ysoft2000@gmail.com>
2025-10-26 18:01:43 +0300

    metal-iso

--- create-talos-xcp.sh
+++ create-talos-xcp.sh
@@ -206,16 +206,10 @@ create_seed_iso_from_mc() {
   yq '.machine.network.hostname = "'"${vmname}"'"' | \
   yq '.machine.network.interfaces[0].routes[0].gateway = "'"${GATEWAY}"'"' | \
   yq '.machine.network.nameservers[0] = "'"${DNS_SERVER}"'"' | \
-  yq '.machine.network.interfaces[0].addresses[0] = "'"$ip_cidr"'"' > "${src_dir}/user-data"
-
-  # meta-data
-  cat > "${src_dir}/meta-data" <<EOF
-instance-id: ${vmname}
-local-hostname: ${vmname}
-EOF
+  yq '.machine.network.interfaces[0].addresses[0] = "'"$ip_cidr"'"' > "${src_dir}/config.yaml"
 
   # Сборка ISO
-  genisoimage -quiet -volid metal-iso -joliet -rock -o "$out_iso" -graft-points "user-data=${src_dir}/user-data" "meta-data=${src_dir}/meta-data"
+  genisoimage -joliet -rock -volid 'metal-iso' -output "$out_iso" "${src_dir}/"
 
   echo "$out_iso"
 }
