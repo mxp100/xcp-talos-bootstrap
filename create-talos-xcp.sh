2546b5523ff7c97ac5e56dc3838f9a77c0c26129
yuriy <ysoft2000@gmail.com>
2025-11-06 20:33:50 +0300

    fix

--- create-talos-xcp.sh
+++ create-talos-xcp.sh
@@ -265,7 +265,7 @@ create_seed_iso_from_mc() {
 
   if [[ "$role" == "cp" ]]; then
     config=$(echo "$config" | \
-      yq '.machine.network.interfaces[0].vip.ip = "'"${VIP_IP}"'"'
+      yq '.machine.network.interfaces[0].vip.ip = "'"${VIP_IP}"'"' | \
       yq '.cluster.network.cni.name = "none"'
     )
 
