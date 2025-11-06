e95b5f7f2556b04db2e0fc95c4d3218ab81fa7d8
yuriy <ysoft2000@gmail.com>
2025-11-06 20:24:21 +0300

    fix

--- create-talos-xcp.sh
+++ create-talos-xcp.sh
@@ -266,7 +266,7 @@ create_seed_iso_from_mc() {
   if [[ "$role" == "cp" ]]; then
     config=$(echo "$config" | \
       yq '.machine.network.interfaces[0].vip.ip = "'"${VIP_IP}"'"'
-#      yq '.cluster.network.cni.name = "none"'
+      yq '.cluster.network.cni.name = "none"'
     )
 
     # Add control plane IPs to certSANs
